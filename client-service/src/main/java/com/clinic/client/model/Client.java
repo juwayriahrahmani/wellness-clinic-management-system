f6e08ee48b811efce880d8b3fac7d5ca4166d8f8
juwayriahrahmani <65194328+juwayriahrahmani@users.noreply.github.com>
2025-07-20 15:42:27 +0530

    Add client-service/src/main/java/com/clinic/client/model/Client.java 1753006051776

---
+++ client-service/src/main/java/com/clinic/client/model/Client.java
@@ -0,0 +1,80 @@
+package com.clinic.client.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    private String id;
+
+    @NotBlank(message = "Name is required")
+    @Column(nullable = false)
+    private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank(message = "Phone is required")
+    @Column(nullable = false)
+    private String phone;
+
+    // Default constructor
+    public Client() {}
+
+    // Constructor with parameters
+    public Client(String id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
